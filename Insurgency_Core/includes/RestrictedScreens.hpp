39fd2dc7bb28cc9563798e3887ca8875879f2178
Bosenator <strooptroopers+ghbose@gmail.com>
2017-09-01 01:44:27 +0100

    Centralise Description.ext as much as possible

---
+++ Insurgency_Core/includes/RestrictedScreens.hpp
@@ -0,0 +1,17 @@
+class Restricted_Pilot {
+	title = "Transport Pilot Unavailable";
+	subtitle = "More players are needed for Pilot.";
+	description = "5 players are required, in order to take this slot.";
+};
+
+class Restricted_Armour {
+	title = "Armour Crew Unavailable";
+	subtitle = "More players are needed for Armour Crew.";
+	description = "12 players are required, in order to take this slot.";
+};
+
+class Restricted_Jet {
+	title = "Jet Unavailable";
+	subtitle = "More players are needed for a Jet.";
+	description = "15 players are required, in order to take this slot.";
+};
---
+++ Insurgency_Core/includes/VictoryScreens.hpp
@@ -0,0 +1,5 @@
+class pointVictory {
+	title = "Mission Accomplished";
+	subtitle = "We did it, TWC";
+	description = "We've been able to quell the local insurgency, by winning over the populace and destablising insurgent efforts. Well done!";
+};
